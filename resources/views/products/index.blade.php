0a52a6e22c5fd6118657e2945305b0be05e5ca5f
Lalitnagesh <lalitnagesh143@gmail.com>
2023-07-25 00:48:46 +0530

    crud
    25/07/23
    00 : 47
    
    Co-Authored-By: AnkitaRajpoot <109818720+AnkitaRajpoot@users.noreply.github.com>

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,22 @@
+@extends('products.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD Example from scratch - ItSolutionStuff.com</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products');
 });
+use App\Http\Controllers\ProductController;
+  
+Route::resource('products', ProductController::class);
